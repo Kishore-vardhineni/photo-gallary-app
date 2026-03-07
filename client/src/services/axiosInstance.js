207b38ae645fb81394675e41c07527a1d9afb66c
kishor <kishore.vardhineni01@gmail.com>
2026-03-07 15:19:17 +0530

    localhost update changes

--- client/src/services/axiosInstance.js
+++ client/src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://13.127.122.73:3001/api",
+    baseURL: "http://localhost:3001/api",
     headers: {
         "Content-Type": "application/json"
     }
