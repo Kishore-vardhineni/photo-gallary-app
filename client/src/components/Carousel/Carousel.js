98a46c0b2c96e4d1e73f7fb1f74f3c02debfc570
kishor <kishore.vardhineni01@gmail.com>
2026-03-28 15:33:25 +0530

    all images hd quality

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import img1 from "../../assets/images/sriramnavami.jpg";
-import img2 from "../../assets/images/ugadi.jpg";
+import img2 from "../../assets/images/carousel2.jpg";
 import img3 from "../../assets/images/carousel3.jpg";
 import img4 from "../../assets/images/carousel4.jpg";
 
