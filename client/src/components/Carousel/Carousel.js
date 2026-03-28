378732c0894f9acce84e2c8ea56f548e85c3dba5
kishor <kishore.vardhineni01@gmail.com>
2026-03-28 16:15:20 +0530

    all photos updated

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import img1 from "../../assets/images/sriramnavami.png";
+import img1 from "../../assets/images/sriramnavami.jpg";
 import img2 from "../../assets/images/carousel1.jpg";
-import img3 from "../../assets/images/carousel3.png";
+import img3 from "../../assets/images/carousel3.jpg";
 import img4 from "../../assets/images/carousel4.jpg";
 
 const images = [img1, img2, img3, img4];
